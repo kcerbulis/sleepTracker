2efb3c86022129167d364bc0db060e8433e2cb1b
kcerbulis <kcerbulis@gmail.com>
2021-04-21 17:07:37 +0100

    time spent sleeping shown, need more work

--- app/src/main/java/com/example/sleeptracker/userDataInputActivity.java
+++ app/src/main/java/com/example/sleeptracker/userDataInputActivity.java
@@ -2,19 +2,24 @@ package com.example.sleeptracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
+
 import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class userDataInputActivity extends AppCompatActivity {
     private TimePicker timePicker1;
     private TextView time;
+    private TextView curTimeText, hourTxt, minTxt, timeSleeping;
     private Calendar calendar;
     private String format = "";
 
@@ -30,12 +35,74 @@ public class userDataInputActivity extends AppCompatActivity {
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int min = calendar.get(Calendar.MINUTE);
         showTime(hour, min);
+
+
+        //Gets textbox to display current time
+        curTimeText = (TextView) findViewById(R.id.curTime);
+
+        timeSleeping =  (TextView) findViewById(R.id.timeSleep);
+
+
     }
+
+
+
+
+
+
+    public void openThirdView(View view) {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
+
+
 
     public void setTime(View view) {
         int hour = timePicker1.getCurrentHour();
         int min = timePicker1.getCurrentMinute();
-        showTime(hour, min);
+
+
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+
+
+        //Displays current time
+        String currentTime = Calendar.getInstance().getTime().toString();
+        curTimeText.setText(formatter.format(calendar.getTime()));
+
+        //Slices time string for calculation
+        String curTimeHourString = (String) curTimeText.getText().subSequence(0, 2);
+        String curTimeMinString = (String) curTimeText.getText().subSequence(3, 5);
+
+        int curTimeHour = Integer.parseInt(curTimeHourString);
+        int curTimeMin = Integer.parseInt(curTimeMinString);
+
+        int hourSleep = hour - curTimeHour;
+        int minSleep = min - curTimeMin;
+
+        if(hourSleep <= 0){
+            hourSleep = 12 + (-1 * (curTimeHour - hour));
+        }
+
+        if((minSleep) < 0){
+            int minSleepReamain = minSleep;
+            hourSleep--;
+            minSleep = 60 + minSleepReamain;
+        }
+
+        if(String.valueOf(minSleep).length() == 1){
+            timeSleeping.setText(new StringBuilder().append(hourSleep).append(" : 0").append(minSleep)
+                    .append(" "));
+        }
+        else {
+            timeSleeping.setText(new StringBuilder().append(hourSleep).append(" : ").append(minSleep)
+                    .append(" "));
+        }
     }
 
     public void showTime(int hour, int min) {
